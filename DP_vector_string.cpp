0ea878069f7c6824f21ee676d833700dd5a19d63
YoungWin <59108730+YoungWin@users.noreply.github.com>
2020-07-26 11:05:18 +0800

    Update DP_vector_string.cpp

--- DP_vector_string.cpp
+++ DP_vector_string.cpp
@@ -252,6 +252,41 @@ public:
 };
 
 /*
+647. 回文子串
+给定一个字符串，你的任务是计算这个字符串中有多少个回文子串。
+具有不同开始位置或结束位置的子串，即使是由相同的字符组成，也会被计为是不同的子串。
+示例 1:
+输入: "abc"
+输出: 3
+解释: 三个回文子串: "a", "b", "c".
+示例 2:
+输入: "aaa"
+输出: 6
+说明: 6个回文子串: "a", "a", "a", "aa", "aa", "aaa".
+注意:
+输入的字符串长度不会超过1000。
+*/
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int N=s.size();
+        if(N<2) return N;
+        vector<vector<bool>> dp(N, vector<bool>(N));
+        int cnt=0;
+        
+        for(int j=0; j<N; j++){
+            for(int i=j; i>=0; i--){
+                if((s[i]==s[j] && j-i<=2)||(s[i]==s[j] && dp[i+1][j-1])){
+                    dp[i][j]=true;
+                    cnt+=1;
+                }
+            }
+        }
+        return cnt;
+    }
+};
+
+/*
 516. 最长回文子序列【不连续的字母序列】
 给定一个字符串 s ，找到其中最长的回文子序列，并返回该序列的长度。可以假设 s 的最大长度为 1000 。
 示例 1:
